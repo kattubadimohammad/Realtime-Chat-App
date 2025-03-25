96f5502e19f16c89b16259b9f8215b3499e12959
K Mohammad <95924696+kattubadimohammad@users.noreply.github.com>
2025-03-26 00:49:26 +0530

    Update index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,20 +1,38 @@
-// node server which will handle socket io connections
-const io = require("socket.io")(8000);
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
 const users = {};
 
- io.on('connection', socket=>{
-     socket.on('new-user-joined', name=>{
-         console.log("New user", name);
-         users[socket.id] = name;
-         socket.broadcast.emit('user-joined', name);
-     });
+// Serve static files from the root (for index.html, CSS, JS)
+app.use(express.static(__dirname + '/../'));
 
-     socket.on('send', message=>{
-         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-     });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/../index.html');
+});
 
-     socket.on('disconnect', message=>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
- })
+io.on('connection', (socket) => {
+  socket.on('new-user-joined', (name) => {
+    console.log("New user:", name);
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  socket.on('send', (message) => {
+    socket.broadcast.emit('receive', { message, name: users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
