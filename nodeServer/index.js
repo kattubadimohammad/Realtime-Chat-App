d0949e119f9db0af9a33ff35296db05fe2e50302
K Mohammad <95924696+kattubadimohammad@users.noreply.github.com>
2025-03-26 12:48:13 +0530

    Update index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,35 +1,16 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST']
-  }
-});
-
-const users = {};
-
-// Serve static files from the root (for index.html, CSS, JS)
-app.use(express.static(__dirname + '/../'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/../index.html');
-});
-
 io.on('connection', (socket) => {
+  console.log("New connection established", socket.id);
+  
   socket.on('new-user-joined', (name) => {
     if (name) {
-      console.log("New user joined:", name);
+      console.log(`User connected: ${name}, Socket ID: ${socket.id}`);
       users[socket.id] = name;
       socket.broadcast.emit('user-joined', name);
     }
   });
 
   socket.on('send', (message) => {
+    console.log(`Message from ${users[socket.id]}: ${message}`);
     if (message && users[socket.id]) {
       socket.broadcast.emit('receive', { message, name: users[socket.id] });
     }
@@ -37,13 +18,9 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     if (users[socket.id]) {
+      console.log(`User disconnected: ${users[socket.id]}, Socket ID: ${socket.id}`);
       socket.broadcast.emit('left', users[socket.id]);
       delete users[socket.id];
     }
   });
-});
-
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
 });
